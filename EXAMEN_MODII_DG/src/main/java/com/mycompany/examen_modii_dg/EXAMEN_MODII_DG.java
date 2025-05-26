b27adaf5b58ea9bc37df491e9c6e0cb2d2759795
tata <0333232012@unjfsc.edu.pe>
2025-05-26 14:04:54 -0500

    Add files via upload

---
+++ EXAMEN_MODII_DG/src/main/java/com/mycompany/examen_modii_dg/EXAMEN_MODII_DG.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examen_modii_dg;
+
+/**
+ *
+ * @author rosar
+ */
+public class EXAMEN_MODII_DG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
